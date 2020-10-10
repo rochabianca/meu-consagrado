e6dcaa04ebf116b8b93496d0fd93ec9a703040ad
rochabianca <bianca.rocha1711@gmail.com>
2020-10-10 15:20:38 -0300

    navbar style

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <template>
   <div id="app">
-    <router-view />
+    <navbar />
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+body {
+  margin: 0;
+  ul,
+  li {
+    margin: 0px;
+    padding: 0;
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+  }
+}
+.container {
+  padding: 32px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="navbar">
+    <img :src="require('@/assets/images/logo.png')" class="navbar__logo" />
+    <nav class="navbar__nav">
+      <ul class="navbar__list">
+        <li class="navbar__item">
+          <router-link to="/">Home</router-link>
+        </li>
+        <li class="navbar__item">
+          <router-link to="/about">Sobre</router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  background: $red;
+  padding: 8px 32px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .navbar__logo {
+    width: 180px;
+  }
+  .navbar__nav {
+    .navbar__list {
+      display: flex;
+      align-items: center;
+      .navbar__item a {
+        color: $white;
+        transition: all ease 0.3s;
+        &:hover,
+        &.router-link-exact-active {
+          color: $dark-blue;
+        }
+      }
+      .navbar__item + .navbar__item {
+        margin-left: 16px;
+      }
+    }
+  }
+}
+</style>

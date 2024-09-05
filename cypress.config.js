0581ad101101869c0dcd2e1086f891a7969e8a0b
Bianca <bianca.rocha1711@gmail.com>
2024-09-05 16:46:06 -0300

    adding eslint file

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require("cypress");
-
+const codeCoverage = require("@cypress/code-coverage/task");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config)
+      // @ts-ignore
+      codeCoverage(on, config);
       // implement node event listeners here
-      return config
+      return config;
     },
-    baseUrl: 'http://localhost:8080/',
-    experimentalRunAllSpecs: true,
-  },
+    baseUrl: "http://localhost:8080/",
+    experimentalRunAllSpecs: true
+  }
 });
--- src/components/PayBill.vue
+++ src/components/PayBill.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script>
-import Modal from "./Modal";
+import Modal from "./Modal.vue";
 
 export default {
   name: "PayBill",
